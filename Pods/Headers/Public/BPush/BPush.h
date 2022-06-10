84f652047752437e563194e65fd58bff49918dc9
Cult <ioscultlee@gmail.com>
2022-06-10 13:42:57 +0800

    删除ignore

---
+++ Pods/Headers/Public/BPush/BPush.h
@@ -0,0 +1,1 @@
+../../../BPush/File/normalversion/BPush.h
---
+++ Pods/Target Support Files/SimpleAudioPlayer/SimpleAudioPlayer-umbrella.h
@@ -0,0 +1,17 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#else
+#ifndef FOUNDATION_EXPORT
+#if defined(__cplusplus)
+#define FOUNDATION_EXPORT extern "C"
+#else
+#define FOUNDATION_EXPORT extern
+#endif
+#endif
+#endif
+
+#import "SimpleAudioPlayer.h"
+
+FOUNDATION_EXPORT double SimpleAudioPlayerVersionNumber;
+FOUNDATION_EXPORT const unsigned char SimpleAudioPlayerVersionString[];
+
